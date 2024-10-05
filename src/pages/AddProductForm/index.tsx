565ac03954eac8958cfab1e67e85a9e6609b9679
Neeraj Agrawal <neeraj.agrawal@accolitedigital.com>
2024-10-05 16:16:31 +0530

    formatted the code

--- src/pages/AddProductForm/index.tsx
+++ src/pages/AddProductForm/index.tsx
@@ -1,86 +1,92 @@
-import { useState, useEffect } from 'react';
-import './style.css';
-import { getOrderDetails, Order, OrderDetail } from '@acc/api';
-import { useAddItem } from '../../hooks/useAddItem';
+import { useState, useEffect } from "react";
+import "./style.css";
+import { getOrderDetails, Order, OrderDetail } from "@acc/api";
+import { useAddItem } from "../../hooks/useAddItem";
 
 interface AddProductFormProps {
-    cartId?: string;
+  cartId?: string;
 }
 
 export default function AddProductForm(props: AddProductFormProps) {
-    const [isLoading, setLoading] = useState<boolean>(true);
-    const [orders, setOrders] = useState<OrderDetail | null>(null);
+  const [isLoading, setLoading] = useState<boolean>(true);
+  const [orders, setOrders] = useState<OrderDetail | null>(null);
 
-    const onItemAddedToCart = () => {
-        location.href = '#';
-    }
+  const onItemAddedToCart = () => {
+    location.href = "#";
+  };
 
-    const { addItemToCart } = useAddItem(onItemAddedToCart);
+  const { addItemToCart } = useAddItem(onItemAddedToCart);
 
-    useEffect(() => {
-        setTimeout(async () => {
-            //set time out added to get the loading effects 
-            const orderData = await getOrderDetails(Math.floor(Math.random() * 10) + 1);
-            setOrders(orderData);
-            setLoading(false);
-        }, 100);
-    }, []);
+  useEffect(() => {
+    setTimeout(async () => {
+      //set time out added to get the loading effects
+      const orderData = await getOrderDetails(
+        Math.floor(Math.random() * 10) + 1
+      );
+      setOrders(orderData);
+      setLoading(false);
+    }, 100);
+  }, []);
 
-    const onAddBtnClicked = (product: Order) => {
-        addItemToCart(product, props.cartId);
-    }
+  const onAddBtnClicked = (product: Order) => {
+    addItemToCart(product, props.cartId);
+  };
 
-    return (
-        <div id="add-cart-item" className='modal'>
-            <div className='content'>
-                <div>
-                    <h2>Add Item in the Cart</h2>
-                    <a href='#' className='close'>&times;</a>
-                </div>
-                <div>
-                    {
-                        isLoading ?
-                            <div id="loader" style={{ marginTop: '30px', marginBottom: '30px' }}></div>
-                            :
-                            <div className='scrollBarForm'>
-                                {
-                                    orders?.products &&
-                                    orders.products.map(order => {
-                                        return (
-                                            <div key={order.id}>
-                                                <div className='users'>
-                                                    <div id="detailSection">
-                                                        <img src={order?.thumbnail} width={80} height={80} />
-                                                    </div>
-                                                    <div style={{ flex: 1 }}>
-                                                        <div id="detailSection">
-                                                            <label id="fieldOrder">Product:
-                                                            </label>
-                                                            <label id="valueOrder">
-                                                                {order?.title}
-                                                            </label>
-                                                        </div>
-                                                        <div id="detailSection">
-                                                            <label id="fieldOrder">Quantity:</label>
-                                                            <label id="valueOrder">{order?.quantity}</label>
-                                                        </div>
-                                                        <div id="detailSection">
-                                                            <label id="fieldOrder">Price:</label>
-                                                            <label id="valueOrder">{order?.price}</label>
-                                                        </div>
-                                                    </div>
-                                                    <div>
-                                                        <button className='addToCartBtn' onClick={() => onAddBtnClicked(order)}>⨁ Add Item</button>
-                                                    </div>
-                                                </div>
-                                            </div>
-                                        )
-                                    })
-                                }
-                            </div>
-                    }
-                </div>
-            </div>
+  return (
+    <div id="add-cart-item" className="modal">
+      <div className="content">
+        <div>
+          <h2>Add Item in the Cart</h2>
+          <a href="#" className="close">
+            &times;
+          </a>
         </div>
-    )
+        <div>
+          {isLoading ? (
+            <div
+              id="loader"
+              style={{ marginTop: "30px", marginBottom: "30px" }}
+            ></div>
+          ) : (
+            <div className="scrollBarForm">
+              {orders?.products &&
+                orders.products.map((order) => {
+                  return (
+                    <div key={order.id}>
+                      <div className="users">
+                        <div id="detailSection">
+                          <img src={order?.thumbnail} width={80} height={80} />
+                        </div>
+                        <div style={{ flex: 1 }}>
+                          <div id="detailSection">
+                            <label id="fieldOrder">Product:</label>
+                            <label id="valueOrder">{order?.title}</label>
+                          </div>
+                          <div id="detailSection">
+                            <label id="fieldOrder">Quantity:</label>
+                            <label id="valueOrder">{order?.quantity}</label>
+                          </div>
+                          <div id="detailSection">
+                            <label id="fieldOrder">Price:</label>
+                            <label id="valueOrder">{order?.price}</label>
+                          </div>
+                        </div>
+                        <div>
+                          <button
+                            className="addToCartBtn"
+                            onClick={() => onAddBtnClicked(order)}
+                          >
+                            ⨁ Add Items
+                          </button>
+                        </div>
+                      </div>
+                    </div>
+                  );
+                })}
+            </div>
+          )}
+        </div>
+      </div>
+    </div>
+  );
 }
